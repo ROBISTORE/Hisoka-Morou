1341a5c44adaf021c633dafd2ac3e82e85d13f96
ROBISTORE <107118851+ROBISTORE@users.noreply.github.com>
2022-10-20 16:29:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6281278803117','6282182477632','6281268176786','']
+global.premium = ['6281278803117']
+global.packname = 'ROBI BOTZ'
+global.author = 'ROBI HOSTING'
+global.sessionName = 'robi'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
